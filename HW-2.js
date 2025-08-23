67f7058a740099086dc6281ce2e1a3f8e8ef599e
Emmanueeel <imaanii@mail.ru>
2025-08-23 18:28:28 +0300

    изменила

---
+++ HW-2.js
@@ -0,0 +1,57 @@
+// Задание1 
+let a= 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+// Задание 2
+const year = 2007
+alert(year)
+
+// Задание 3
+const creator = 'Брендан Эйх'
+alert(creator);
+
+// Задание 4
+let a1 = 10;
+let b = 2;
+alert(a1 + b);
+alert(a1 - b);
+alert(a1 * b);
+alert(a1 / b);
+
+// Задание 5
+let result = 2 ** 5;
+alert(result);
+
+// Задание 6
+a = 9;
+b = 2;
+result = a % b;
+alert(result);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+const age = prompt('Сколько вам лет?');
+alert(`Вам ${age} лет`);  
+
+// Задание 9
+const user1 = {
+    name: 'Anna',
+    age: '30',
+    isAdmin: true
+};
+
+// Задание 10
+let user = prompt('Как вас зовут?');
+alert(`Привет, ${user} !`);
